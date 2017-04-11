481de149af55ddbf2d771221f6e89c8520d1b1ac
nyx <sakusennx@gmail.com>
2017-04-11 14:23:38 +0900

    commit test

---
+++ src/application/MainApp.java
@@ -0,0 +1,108 @@
+package application;
+
+
+
+import beans.Person;
+import controller.PersonOverviewController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+/**
+ * @author nyx
+ *
+ */
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
+
+		initRootLayOut();
+		showPersonOverView();
+	}
+
+
+	/*
+	 *Initializes the root layout.
+	 */
+	public void initRootLayOut(){
+		try{
+			//Load root layout from fxml file.
+			System.out.println(MainApp.class.getResource("view/RootLayout.fxml"));
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane)loader.load();
+			//Show the scene containing the root layout
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);//not resize
+			primaryStage.show();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 *	Initializes the person overview inside the root layout.
+	 */
+	public void showPersonOverView(){
+		try{
+			//load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverView = (AnchorPane)loader.load();
+			//Set person over view into the center of root  layout.
+			rootLayout.setCenter(personOverView);
+
+
+			  // Give the controller access to the main app.
+	        PersonOverviewController controller = loader.getController();
+	        controller.setMainApp(this);
+
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public Stage getPrimaryStage(){
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * constluctor
+	 */
+	public  MainApp(){
+		//Add some sampledata
+		personData.add(new Person("a","b") );
+		personData.add(new Person("c","d") );
+		personData.add(new Person("e","f") );
+		personData.add(new Person("g","h") );
+		personData.add(new Person("i","j") );
+	}
+	/**
+     * Returns the data as an observable list of Persons.
+     * @return
+     */
+	 public ObservableList<Person> getPersonData() {
+	        return personData;
+	    }
+}
---
+++ src/controller/PersonOverviewController.java
@@ -0,0 +1,131 @@
+package controller;
+
+import application.MainApp;
+import beans.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import logic.util.DateUtil;
+
+public class PersonOverviewController {
+	@FXML
+	private TableView<Person> personTable;
+	@FXML
+	private TableColumn<Person,String> firstNameColumn;
+	@FXML
+	private TableColumn<Person,String> lastNameColumn;
+
+	@FXML
+	private Label firstNameLabel;
+	@FXML
+	private Label lastNameLabel;
+	@FXML
+	private Label streetLabel;
+	@FXML
+	private Label postalCodeLabel;
+	@FXML
+	private Label cityLabel;
+	@FXML
+	private Label birthdayLabel;
+
+	@FXML
+	private Button deleteButton;
+
+	//Reference to the main application.
+	private MainApp mainApp;
+
+	// it is run after Inititialize()
+	public PersonOverviewController(){
+
+	}
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize(){
+		 // Initialize the person table with the two columns.
+		// I need  to know cellvalueFactory.
+		firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+		lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+		//Clear person details.
+		showPersonDetails(null);
+		//Listen for selection changes and show the person details when changed.
+		personTable.getSelectionModel().selectedItemProperty().addListener(
+			(observable,oldValue,newValue)->showPersonDetails(newValue));
+
+	}
+	//解析中↓
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 *
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp ){
+		this.mainApp = mainApp;
+		// Add observable list data to the table
+		personTable.setItems(mainApp.getPersonData());
+	}
+
+
+	/**
+	 *Fills all text fields to show details about the person.
+	 *if the specified is null , all text fields are cleared.
+	 *
+	 * @param person the person or null
+	 */
+	private void showPersonDetails(Person person){
+		if(person != null){
+			//fill the labels with info from the person object.
+			firstNameLabel.setText(person.getFirstName());
+			lastNameLabel.setText(person.getLastName());
+			streetLabel.setText(person.getStreet());
+			postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+			cityLabel.setText(person.getCity());
+			birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+
+			//TODO::  we need a way to convert the birthday  into a String.
+			//birthdayLabel.setText(person.getBirthday(...));
+		}else{
+			//person is null, remove  all the text
+			firstNameLabel.setText("");
+			lastNameLabel.setText("");
+			postalCodeLabel.setText("");
+			streetLabel.setText("");
+			cityLabel.setText("");
+			birthdayLabel.setText("");
+
+		}
+	}
+
+	/**
+	 * Called when the user click on the delete button.
+	 */
+	@FXML
+	private  void handleDeletePerson( ){
+		int selctedIndex = personTable.getSelectionModel().getSelectedIndex();
+		try{
+			if(selctedIndex >=0){
+		personTable.getItems().remove(selctedIndex);
+			}else{
+				//nothing selected.
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.initOwner(mainApp.getPrimaryStage());
+				alert.setTitle("No Selection.");
+				alert.setHeaderText("No Person Selected");
+				alert.setContentText("Please select a person in the table.");
+
+				alert.showAndWait();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	//private void handle
+
+}
